4ea6201bd2ebf04503ac362ae207cb480824336e
Joanderson <sirjoandersonwf@gmail.com>
2024-04-18 03:53:29 -0300

    add: outline to the gig card

--- components/GigCard.vue
+++ components/GigCard.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex flex-col p-3 bg-white gap-3">
+  <div
+    class="flex flex-col p-3 bg-white outline outline-1 rounded-lg outline-gray-300 gap-3"
+  >
     <div>
       <div class="flex text-2xl justify-between gap-2">
         <span class="font-semibold">{{ title }}</span>
